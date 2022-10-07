5c706d38757a70f6c3c6fb1798781d4dc17752f4
himanshu3112 <himanshu312017gmail.com>
2022-10-07 15:08:03 +0530

    Hello

---
+++ Stock_Game/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('',views.index,name="home"),
+    path('stocks/',views.stocks_list,name="stocks"),
+    path('portfolio/<team_name>/',views.portfolio,name="stocks"),
+    path('ranklist/<team_name>/',views.ranklist,name="stocks"),
+    path('stockname/<stock>/',views.stockers,name="stockname"),
+    path('stock_details/', views.stock_details, name='stock_details')
+
+]
