1ca999d9e8fa34976085ee446ff3fcbd4cf092db
Rohit <2019rohit.karalkar@ves.ac.in>
2022-10-08 08:52:29 +0530

    Rohit

--- Stock_Game/urls.py
+++ Stock_Game/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.index,name="home"),
     path('stocks/',views.stocks_list,name="stocks"),
     path('stockname/<stock>/',views.stockers,name="stockname"),
-    path('stock_details/', views.stock_details, name='stock_details')
-
+    path('<room_name>/<stock_name>/', views.stock_details, name='stock_details'),
+    path('<room_name>/<stock_name>/<current_stock_price>/', views.push_details, name='push_details'),
+    path('<room_name>/<stock_name>/<current_stock_price>/<no_of_shares>/', views.pull_details, name='pull_details')
 ]
