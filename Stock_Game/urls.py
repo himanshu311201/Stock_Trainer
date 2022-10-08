31dd89ef92b8c5951bfee88bc99d1407b3506ef3
himanshu3112 <himanshu312017gmail.com>
2022-10-08 09:00:36 +0530

    Added url

--- Stock_Game/urls.py
+++ Stock_Game/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('createform/',views.Create_team,name="creater"),
     path('showname/<team_hel>',views.shownum,name="show"),
     path('join/',views.join,name="join"),
+    path('<room_name>/<stock_name>/', views.stock_details, name='stock_details'),
+    path('<room_name>/<stock_name>/<current_stock_price>/', views.push_details, name='push_details'),
+    path('<room_name>/<stock_name>/<current_stock_price>/<no_of_shares>/', views.pull_details, name='pull_details')
     path('stock_details/', views.stock_details, name='stock_details')
 
 ]
